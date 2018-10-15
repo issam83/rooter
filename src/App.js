ee3b865feda63815e7035d9529d4197c5f8ba3a4
issam83 <elagrebiissam@gmail.com>
2018-10-15 17:31:34 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter ,Switch, NavLink, Route} from "react-router-dom"
+import Home from './Home';
+import History from './History';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     
+      <BrowserRouter>
+       <div className="style">
+        
+             <NavLink className="title" activeClassName="active"
+               exact to="/"
+               activeStyle={{ color: "red" }}
+               >Bienvenue
+               </NavLink>
+      
+             <NavLink activeClassName="active"
+              exact to="/notre-histoire"
+              activeStyle={{ color: "red" }}
+              >Notre-histoire</NavLink>
+      
+         <Switch>
+           <Route exact path="/" component={Home} />
+           <Route path="/notre-histoire" activeClassName="selected" component={History} />
+         </Switch>
+         </div>
+       
+      </BrowserRouter>
     );
   }
 }
---
+++ src/History.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class History extends Component {
+  render() {
+    return (
+      <div>
+          <h2>Notre histoire</h2>
+      </div>
+    )
+  }
+}
